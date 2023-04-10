416028210843f854e6b0d6a69867bcd876682a25
Darius Ng <darius-ng@live.com>
2023-04-10 20:44:47 +0800

    Update ReadOnlyFitBook.java

--- src/main/java/seedu/fitbook/model/ReadOnlyFitBook.java
+++ src/main/java/seedu/fitbook/model/ReadOnlyFitBook.java
@@ -2,12 +2,13 @@ package seedu.fitbook.model;
 
 import javafx.collections.ObservableList;
 import seedu.fitbook.model.client.Client;
-
+//@@author
 /**
  * Unmodifiable view of FitBook
  */
 public interface ReadOnlyFitBook {
-
+    
+    //@@author
     /**
      * Returns an unmodifiable view of the clients list.
      * This list will not contain any duplicate clients.
